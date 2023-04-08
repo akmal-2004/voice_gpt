0ac1c963ec144b799ce466b3f20b5010d8d17073
Akmal <akmallatifov2004@gmail.com>
2023-04-08 16:15:34 +0500

    initial

---
+++ main.py
@@ -0,0 +1,35 @@
+import openai
+
+import config
+import stt
+import tts
+
+openai.api_key = (config.API_KEY)
+
+def get_response(prompt):
+    response = openai.Completion.create(
+        model = "text-davinci-003",
+        # prompt = "I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with \"Unknown\".\n\nQ: What is human life expectancy in the United States?\nA: Human life expectancy in the United States is 78 years.\n\nQ: Who was president of the United States in 1955?\nA: Dwight D. Eisenhower was president of the United States in 1955.\n\nQ: Which party did he belong to?\nA: He belonged to the Republican Party.\n\nQ: What is the square root of banana?\nA: Unknown\n\nQ: How does a telescope work?\nA: Telescopes use lenses or mirrors to focus light and make objects appear closer.\n\nQ: Where were the 1992 Olympics held?\nA: The 1992 Olympics were held in Barcelona, Spain.\n\nQ: How many squigs are in a bonk?\nA: Unknown\n\nQ: Where is the Valley of Kings?\nA:",
+        prompt = "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\nHuman: " + prompt + "\nAI: ",
+        temperature = 0.8,
+        max_tokens = 500,
+        top_p = 1,
+        frequency_penalty = 0.0,
+        presence_penalty = 0.6,
+        stop=["AI:"]
+    )
+    return response.choices[0].text
+
+def va_respond(voice: str):
+    if 'алекс' in voice:
+        voice = voice.replace('алекс', "").strip()
+        response = get_response(voice)
+        tts.va_speak(response)
+
+
+def main():
+    stt.va_listen(va_respond)
+
+if __name__ == '__main__':
+    va_respond("алекс попреветствуй людей как герой киновселенной - искусственный интеллект. Это должно звучать красиво, умно, креативно, мотивирующе и коротко.")
+    main()
---
+++ tts.py
@@ -0,0 +1,45 @@
+import torch
+import sounddevice as sd
+import time
+from num2t4ru import num2text
+
+language = 'ru'
+model_id = 'v3_1_ru' # ru_v3
+sample_rate = 48000 # 8000, 24000, 48000
+speaker = 'xenia' # aidar, eugene, baya, kseniya, xenia, random
+put_accent = True
+put_yo = True
+device = torch.device('cpu') # cpu или gpu
+text = "Привет, мир!"
+
+model, _ = torch.hub.load(repo_or_dir='snakers4/silero-models',
+                          model='silero_tts',
+                          language=language,
+                          speaker=model_id)
+model.to(device)
+
+
+
+def va_speak(what: str):
+    print(what)
+
+    whatlist = what.replace(':', ' ').replace(',', ' ').replace('-', ' ').split()
+    for i in whatlist:
+        if i.isdigit():
+            what = what.replace(i, num2text(int(i)))
+
+    audio = model.apply_tts(text=what+"..",
+                            speaker=speaker,
+                            sample_rate=sample_rate,
+                            put_accent=put_accent,
+                            put_yo=put_yo)
+
+    sd.play(audio, sample_rate * 1.05)
+    time.sleep((len(audio) / sample_rate) + 0.5)
+    sd.stop()
+
+# sd.play(audio, sample_rate)
+# time.sleep(len(audio) / sample_rate)
+# sd.stop()
+
+# va_speak("привет Акмаль! Это я. Программа, голосовой ассистент! Звучу очень реалистично, не так ли?")
